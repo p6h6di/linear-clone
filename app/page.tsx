55f704e38f0a3dc7e29e67a9a8f4ffd63323f19d
p6h6di <shivamth060@gmail.com>
2025-01-13 18:08:14 +0530

    initialize Next.js project with Tailwind CSS and ESLint configuration

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <header>Header</header>
+      <main>Linear Homepage</main>
+      <footer>Footer</footer>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: '"SF Pro Display", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Open Sans", "Helvetica Neue", sans-serif',
+      },
+      colors: {
+        background: "#000212",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
