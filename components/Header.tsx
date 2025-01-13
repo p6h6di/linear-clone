b3ba157d160eb5212942c93add35c7363e89ebdf
p6h6di <shivamth060@gmail.com>
2025-01-13 22:28:02 +0530

    add Button component and enhance Header with backdrop blur and text shadow utility

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,17 +2,18 @@ import React from "react";
 import Container from "./Container";
 import Link from "next/link";
 import { Logo } from "./icons/logo";
+import Button from "./ui/button";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full border-b border-white-a08">
+    <header className="fixed top-0 left-0 w-full border-b border-white-a08 backdrop-blur-[12px]">
       <Container className="flex h-[var(--navigation-height)]">
         <Link href="/" className="flex items-center text-md">
           <Logo className="size-[1.8rem] mr-4" /> Linear
         </Link>
 
         <nav className="h-full">
-          <ul className="flex items-center [&_a]:text-sm h-full [&_li]:ml-6">
+          <ul className="flex items-center [&_a]:text-sm [&_a:hover]:text-grey [&_a]:transition-colors h-full [&_li]:ml-6">
             <li>
               <Link href="#">Features</Link>
             </li>
@@ -41,7 +42,7 @@ const Header = () => {
           <Link href="#" className="text-sm mr-6">
             Log in
           </Link>
-          <Link href="#">Sign up</Link>
+          <Button href="#">Sign up</Button>
         </div>
       </Container>
     </header>
---
+++ components/ui/button.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/link";
+import { cva, VariantProps } from "class-variance-authority";
+
+interface ButtonProps extends VariantProps<typeof buttonClasses> {
+  children: React.ReactNode;
+  href: string;
+}
+
+const buttonClasses = cva("rounded-full inline-flex items-center", {
+  variants: {
+    variant: {
+      primary: "bg-primary-gradient hover:text-shadow hover:shadow-primary",
+      secondary: "",
+      tertiary: "",
+    },
+    size: {
+      small: "text-xs px-3 h-4",
+      medium: "text-sm px-4 h-8",
+      large: "text-md px-6 h-12",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "medium",
+  },
+});
+
+const Button = ({ children, href, variant, size }: ButtonProps) => {
+  return (
+    <Link href={href} className={buttonClasses({ variant, size })}>
+      {children}
+    </Link>
+  );
+};
+
+export default Button;
