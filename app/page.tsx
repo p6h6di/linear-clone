eb952995b2891dd960f1e3c2676e59cd4d697df9
p6h6di <shivamth060@gmail.com>
2025-01-15 10:27:57 +0530

    enhance Hero component with animation effects for title and subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,11 @@ export default function Home() {
           <span>Linear 2022 Release – Built for scale</span>{" "}
           <Highlight>→</Highlight>
         </Button>
-        <HeroTitle>
+        <HeroTitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
           Linear is a better way <br className="hidden md:block" /> to build
           products
         </HeroTitle>
-        <HeroSubtitle>
+        <HeroSubtitle className="translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
           Meet the new standard for modern software development.
           <br className="hidden md:block" />
           Streamline issues, sprints and product roadmaps.
