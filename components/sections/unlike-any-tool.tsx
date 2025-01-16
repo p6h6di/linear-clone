1cab2c6b73297917374d25fc5d91bd0c952efb7f
p6h6di <shivamth060@gmail.com>
2025-01-16 19:18:31 +0530

    enhance UnlikeAnyTool component with centered layout and descriptive text

--- components/sections/unlike-any-tool.tsx
+++ components/sections/unlike-any-tool.tsx
@@ -3,12 +3,18 @@ import Container from "../Container";
 
 const UnlikeAnyTool = () => {
   return (
-    <div>
+    <div className="text-white">
       <Container>
-        <h2>
-          Unlike any tool
-          <br className="hidden md:inline-block" /> you've used before
-        </h2>
+        <div className="text-center">
+          <h2 className="mb-4 text-4xl md:mb-7 md:text-7xl">
+            Unlike any tool
+            <br className="hidden md:inline-block" /> you've used before
+          </h2>
+          <p className="mx-auto mb-12 max-w-[68rem] text-lg text-primary-text md:mb-7 md:text-xl">
+            Designed to the last pixel and engineered with unforgiving
+            precision, Linear combines UI elegance with world-class performance.
+          </p>
+        </div>
       </Container>
     </div>
   );
